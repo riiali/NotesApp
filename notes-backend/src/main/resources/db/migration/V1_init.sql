624810ed1d93facc032ad23a695288ddf31fdfed
riiali <alrizzo@unibz.it>
2025-09-10 20:41:31 +0200

    chore: added docker and db init

---
+++ notes-backend/src/main/resources/db/migration/V1_init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE notes (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(200) NOT NULL,
+  text TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE INDEX idx_notes_created_at ON notes(created_at);
